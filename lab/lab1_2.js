5c0378d5fc47d0cc311c36a0eb20b5c9add368c9
Berendhard <berendhard@gmail.com>
2018-03-13 11:32:24 +0100

    Working lab 1.2

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -4,7 +4,21 @@ var AWS = require('aws-sdk');
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-	
+
+    ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+
+    var params = {
+    DryRun: false
+    };
+
+    // Call EC2 to retrieve the policy for selected bucket
+    ec2.describeInstances(params, function(err, data) {
+    if (err) {
+        callback(err.stack);
+    } else {
+        callback(null,JSON.stringify(data));
+    }
+    });
 	
 }
 
